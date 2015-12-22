08596df009d822ec9442715f4f701fac9295aad1
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-22 21:18:55 +0000

    fixes to logout.php (request POST, change session ID)
    Do not unset $_SESSION, it does nothing except cause harm.
    
    Do not call session_destroy() or session_write_close(), they prevent session_regenerate_id(true) from working properly.
    
    This endpoint now requires the method used to be POST, and will (eventually) identify the identity provider in the JSON result.

--- logout.php
+++ logout.php
@@ -1,10 +1,24 @@
 <?php
 
+// Require connect.php in case sessions are stored in DynamoDB?
 require_once __DIR__.'/connect.php';
 
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+  http_response_code(405);
+  echo 'Method Not Allowed (use POST)';
+  die;
+}
+
 session_start();
-unset($_SESSION);
 session_unset();
-session_destroy();
-session_write_close();
 session_regenerate_id(true);
+
+header('Content-Type: application/json');
+
+// TODO: if the user was authenticated with their Google+ account,
+// return {"provider":"google"}.
+
+// TODO: if the user was authenticated with their Facebook account,
+// return {"provider":"facebook"}.
+
+echo '{}';
