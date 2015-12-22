0d09ef5a346dec2e6d09fbd69ff4a093dc6e1a37
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-22 15:21:51 +0000

    oauth_authorise: add support for login and already-authorised

--- oauth.sql
+++ oauth.sql
@@ -3,3 +3,4 @@ CREATE TABLE oauth_access_tokens (access_token VARCHAR(40) NOT NULL, client_id V
 CREATE TABLE oauth_authorization_codes (authorization_code VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), redirect_uri VARCHAR(2000), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT auth_code_pk PRIMARY KEY (authorization_code));
 CREATE TABLE oauth_refresh_tokens (refresh_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT refresh_token_pk PRIMARY KEY (refresh_token));
 CREATE TABLE oauth_scopes (scope TEXT, is_default BOOLEAN);
+CREATE TABLE user_authorized_clients (user_id VARCHAR(255), client_id VARCHAR(80) NOT NULL, scope VARCHAR(2000));
--- oauth_authorise.php
+++ oauth_authorise.php
@@ -4,6 +4,17 @@ require_once(__DIR__."/connect.php");
 require_once(__DIR__."/oauth_server.php");
 
 $db = connect();
+
+// Check the session and test whether the user is logged in.
+session_start();
+$login_session = array();
+if (array_key_exists('modules', $_SESSION) &&
+    array_key_exists('login', $_SESSION['modules']))
+  $login_session = $_SESSION['modules']['login'];
+$have_user_id = array_key_exists('idUser', $login_session);
+$user_id = $have_user_id ? $login_session["idUser"] : null;
+
+// Perform OAuth2 request validation and get the client id.
 $server = oauth_server($db);
 $request = OAuth2\Request::createFromGlobals();
 $response = new OAuth2\Response();
@@ -11,35 +22,72 @@ if (!$server->validateAuthorizeRequest($request, $response)) {
     $response->send();
     die;
 }
+$authCtrlr = $server->getAuthorizeController();
+$client_id = $authCtrlr->getClientId();
 
-session_start();
-$login_session = array();
-if (array_key_exists('modules', $_SESSION) &&
-    array_key_exists('login', $_SESSION['modules']))
-  $login_session = $_SESSION['modules']['login'];
-
-// GET request: show the authorization form.
-if (empty($_POST)) {
-  if (!array_key_exists('idUser', $login_session)) {
-    // TODO: show the login form if the user is not already logged in.
-    echo('<!DOCTYPE html><p>TODO: make user log in</p>');
-    die;
+// Test whether the user has already authorized this client for (a subset of)
+// the requested scope.
+$already_authorized = false;
+$stmt = $db->prepare("SELECT `scope` FROM `user_authorized_clients` WHERE `user_id` = :user_id AND `client_id` = :client_id;");
+$stmt->execute(["user_id" => $user_id, "client_id" => $client_id]);
+$scopeUtil = $server->getScopeUtil();
+$required_scope = $authCtrlr->getScope();
+error_log("required scope: ".json_encode($required_scope));
+while ($row = $stmt->fetchObject()) {
+  $available_scope = $row->scope;
+  error_log("available scope: ".json_encode($available_scope));
+  if ($scopeUtil->checkScope($required_scope, $available_scope)) {
+    error_log("we have a match!");
+    $already_authorized = true;
+    break;
   }
-  // TODO: also request user credentials if the query string indicates
-  // that we are requesting authorisation from a specific user, and this
-  // user differs from the logged-in user (if any).
-  $user_login = $login_session["sLogin"];
-  echo('<!DOCTYPE html>
-<form method="POST">
-  <p>Autoriser (XXX client name here) à accéder à votre compte '.htmlspecialchars($user_login).' ?</p>
-  <button type="submit" name="authorized" value="yes">Oui</button>
-  <button type="submit" name="authorized" value="no">Non</button>
-</form>');
+}
+
+if (!empty($_POST)) {
+  // POST request: redirect to client with the authorization code.
+  // XXX We can either get user_id from the session or from the token in
+  // $_GET['jwt'] -- it is not clear which is best.
+  $is_authorized = $have_user_id && $_POST['authorized'] === 'yes';
+  if ($is_authorized && !$already_authorized) {
+    $stmt = $db->prepare("INSERT INTO `user_authorized_clients` (`user_id`, `client_id`, `scope`) VALUES (:user_id, :client_id, :scope)");
+    $stmt->execute(["user_id" => $user_id, "client_id" => $client_id, "scope" => $required_scope]);
+  }
+  $server->handleAuthorizeRequest($request, $response, $is_authorized, $user_id);
+  $response->send();
+  die();
+}
+
+//
+// GET request
+//
+
+// If the user is not logged in, redirect to authorise.html to display the
+// login form and redirect back here once the user has logged in.
+if (!$have_user_id) {
+  header('Location: authorise.html?'.http_build_query([
+    'afterLogin' => $_SERVER['REQUEST_URI']
+  ]));
   die;
 }
 
-// POST request: redirect to client with the authorization code.
-$is_authorized = ($_POST['authorized'] === 'yes');
-$userid = $login_session["idUser"];
-$server->handleAuthorizeRequest($request, $response, $is_authorized, $userid);
-$response->send();
+// Skip showing the authorisation form to the logged-in user if they have
+// already authorised the client for the requested scopes.
+$already_authorised = true;
+
+
+if ($_GET['approval_prompt'] != 'force' && $already_authorised) {
+  $server->handleAuthorizeRequest($request, $response, true, $user_id);
+  $response->send();
+  die();
+}
+
+// Show the authorisation form to the user.
+$client_data = $server->getStorage('client')->getClientDetails($client_id);
+$client_title = $client_data['title'];
+$user_login = $login_session["sLogin"];
+echo('<!DOCTYPE html>
+<form method="POST">
+  <p>Autoriser '.htmlspecialchars($client_title).' à accéder à votre compte '.htmlspecialchars($user_login).' ?</p>
+  <button type="submit" name="authorized" value="yes">Oui</button>
+  <button type="submit" name="authorized" value="no">Non</button>
+</form>');
