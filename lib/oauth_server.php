3985bcd6d475766561ab3987e3d560f87ad2d372
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-03 18:42:51 +0000

    support RefreshToken grant type

--- lib/oauth_server.php
+++ lib/oauth_server.php
@@ -4,5 +4,6 @@ function oauth_server($db) {
   $storage = new OAuth2\Storage\Pdo($db);
   $server = new OAuth2\Server($storage);
   $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+  $server->addGrantType(new OAuth2\GrantType\RefreshToken($storage, []));
   return $server;
 }
