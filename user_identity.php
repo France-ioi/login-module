1efa08064df9874ca071f4ba9b9e02fadfa243b5
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-10 09:23:35 +0000

    allow admins to query any profile

--- user_identity.php
+++ user_identity.php
@@ -16,21 +16,38 @@ if (!$server->verifyResourceRequest($request, $response)) {
     $response->send();
     die;
 }
-$user_id = $token['user_id'];
+$auth_user_id = $token['user_id'];
+
+// TODO: clean this up!
+$stmt = $db->prepare("SELECT `bIsAdmin` FROM `users` WHERE `id` = :user_id");
+$stmt->execute(['user_id' => $auth_user_id]);
+$admin_user = $stmt->fetchObject();
+$is_admin = $admin_user && $admin_user->bIsAdmin;
+
+// Default to querying the authenticated user's profile.
+$query_user_id = $auth_user_id;
+
+// Administrators can query any profile.
+if ($is_admin and array_key_exists('idUser', $_GET)) {
+  $query_user_id = $_GET['idUser'];
+}
 
 $stmt = $db->prepare("SELECT `id`, `sLogin`, `sFirstName`, `sLastName` FROM `users` WHERE `id` = :user_id");
-$stmt->execute(['user_id' => $user_id]);
+$stmt->execute(['user_id' => $query_user_id]);
 $user = $stmt->fetchObject();
+if (!$user) {
+  echo json_encode(['error' => 'no such user']);
+  die();
+}
 
 $stmt = $db->prepare("SELECT `sBadge` FROM `user_badges` WHERE `idUser` = :user_id");
-$stmt->execute(['user_id' => $user_id]);
+$stmt->execute(['user_id' => $query_user_id]);
 $badges = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
 
 echo json_encode([
-  'idUser' => $user_id,
+  'idUser' => $query_user_id,
   'sLogin' => $user->sLogin,
   'sFirstName' => $user->sFirstName,
   'sLastName' => $user->sLastName,
   'badges' => $badges
 ]);
-
