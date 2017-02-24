3f818cb978fea49a77c441db87d49f5d726c4062
mobydimk <mobydimk@gmail.com>
2017-02-24 08:33:04 +0400

    perfect :)

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,8 +18,10 @@
 
 
     @if(session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+        <div class="container">
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
         </div>
     @endif
 @endsection
