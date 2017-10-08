d9df0f83e295fbfec57d4839062b40e2b7ef508a
mobydimk <mobydimk@gmail.com>
2017-10-08 13:24:24 +0400

    profile picture size

--- config/ui.php
+++ config/ui.php
@@ -3,7 +3,7 @@
         'star' => ' (*)',
         'profile_picture' => [
             'default' => env('UI_PROFILE_PICTURE'),
-            'width' => 256,
-            'height' => 256
+            'width' => 200,
+            'height' => 200
         ]
     ];
