f1e5fbee5a85ef375f1718c9e37c1475691615fe
mobydimk <mobydimk@gmail.com>
2017-10-10 13:51:06 +0400

    profile pictures s3 visibility

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,8 @@ return [
             'key' => env('AWS_KEY'),
             'secret' => env('AWS_SECRET'),
             'region' => env('AWS_REGION'),
-            'bucket' => env('AWS_BUCKET')
+            'bucket' => env('AWS_BUCKET'),
+            'visibility' => 'public'
         ],
 
     ],
