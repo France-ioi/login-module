df3084c153defd56ee58b53b31a9a45b3fafc39a
mobydimk <mobydimk@gmail.com>
2018-01-11 17:22:50 +0400

    notification translations

---
+++ resources/lang/de/notifications/email_verification.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'subject' => ":app_name email verification",
+    'body' => "You are receiving this message because a email verification required for your account.\nVerification code: :code"
+];
