24a8fc745d822a9cdf124c7cfb1746f34eaee11e
mobydimk <mobydimk@gmail.com>
2018-01-18 00:28:39 +0400

    HTTP_X_FORWARDED_FOR to detect IP under proxy

--- app/Listeners/AuthLogin.php
+++ app/Listeners/AuthLogin.php
@@ -6,9 +6,13 @@ class AuthLogin
 {
 
     public function handle($event) {
-        $event->user->ip = \Request::ip();
+        $event->user->ip = $this->getIp();
         $event->user->last_login = new \DateTime;
         $event->user->save();
+    }
+
+    private function getIp() {
+        return isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : \Request::ip();
     }
 
 }
