9da87e65eaf2119c655adfc64cad0fa4b3bb0fc7
Michel Blockelet <blockelet@crans.org>
2018-06-12 18:55:35 +0100

    badges: Handle franceioiID both ways

--- resources/lang/fr/badge.php
+++ resources/lang/fr/badge.php
@@ -5,6 +5,7 @@ return [
     'btn_verify_code' => 'Valider',
     'code_verification_fail' => 'La vérification a échoué.',
     'code_registered' => 'Code utilisé par un autre utilisateur',
+    'code_in_use' => "Ce code appartient à l'utilisateur :username, veuillez vous connecter en tant que cet utilisateur avant d'utiliser le code.",
     'do_not_have' => "Je n'ai pas de code",
 
     'badge' => 'Badge',
