c6bca71edd2ffe5a19f6ca0b0a0474c2e6724b3d
Michel Blockelet <blockelet@crans.org>
2018-06-14 23:51:27 +0100

    lang: Modify fr email_verification

--- resources/lang/fr/notifications/email_verification.php
+++ resources/lang/fr/notifications/email_verification.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'subject' => ":app_name - Vérification de l'adresse mail",
-    'body' => "Vous recevez ce message parce qu'une vérification de l'adresse mail est requise pour votre compte.\nCode de vérification : :code"
+    'body' => "Vous recevez ce message parce qu'une vérification de l'adresse mail est requise pour votre compte.\nCode de vérification : :code . Allez dans votre profil utilisateur pour y fournir ce code et nous permettre de valider votre adresse mail."
 ];
