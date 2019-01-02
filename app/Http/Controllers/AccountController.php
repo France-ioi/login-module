3a63996eb4e56618b5b74a2deb287668eb399676
Melanie Storup <m.storup@epixode.fr>
2019-01-02 17:05:40 +0100

    add platform_name in AccountController

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -10,8 +10,10 @@ class AccountController extends Controller
 
 
     public function index(Request $request, PlatformContext $context) {
+        $client = $context->client();
         return view('account.index', [
-            'need_badge_verification' => (bool) $context->badge()->api()
+            'need_badge_verification' => (bool) $context->badge()->api(),
+            'platform_name' => $client ? $client->name : trans('app.name')
         ]);
     }
 
