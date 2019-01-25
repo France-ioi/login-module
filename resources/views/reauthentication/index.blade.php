d3e7157a439307164a0c44ba92e1a78178072a54
Melanie Storup <m.storup@epixode.fr>
2019-01-25 15:15:24 +0100

    style reauthentication and alerts

--- resources/views/reauthentication/index.blade.php
+++ resources/views/reauthentication/index.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
     <div class="panel panel-default">
-        <div class="panel-heading">
-            @lang('reauthentication.header')
+        <div class="alert-section">
+            <div class="alert alert-danger">
+                <i class="fas fa-bell icon"></i>
+                @lang('reauthentication.header')
+            </div>
         </div>
         <div class="panel-body">
-            {!! BootForm::open(['url' => '/reauthentication', 'method' => 'post']) !!}
-                {!! BootForm::password('password', trans('reauthentication.pwd')) !!}
+            {!! BootForm::horizontal(['url' => '/reauthentication', 'method' => 'post']) !!}
+                {!! BootForm::password('password', trans('reauthentication.pwd'), ['placeholder' => trans('reauthentication.pwd'),  'prefix' => BootForm::addonIcon('key fas')]) !!}
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary">
+                    <button type="submit" class="btn btn-primary btn-rounded btn-centered">
+                        <i class="fas fa-check icon"></i>
                         @lang('ui.continue')
                     </button>
                     @if($cancel_url)
-                        <a class="btn btn-link" href="{{ $cancel_url }}">
+                        <a class="btn btn-danger btn-rounded btn-centered" href="{{ $cancel_url }}">
+                            <i class="fas fa-times icon"></i>
                             @lang('ui.close')
                         </a>
                     @endif
