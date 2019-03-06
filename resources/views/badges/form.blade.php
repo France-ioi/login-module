36555b74132244f51babf824da0a4f88c42104c1
Melanie Storup <m.storup@epixode.fr>
2019-03-06 17:25:07 +0100

    style badges form

--- resources/views/badges/form.blade.php
+++ resources/views/badges/form.blade.php
@@ -1,6 +1,6 @@
-{!! BootForm::open(['url' => '/badges/add']) !!}
+{!! BootForm::horizontal(['url' => '/badges/add']) !!}
     {!! BootForm::select('badge_api_id', trans('badges.select_badge_api'), $available) !!}
-    {!! BootForm::text('code', trans('badges.code')) !!}
+    {!! BootForm::text('code', trans('badges.code'), null, ['placeholder' => trans('badges.code'),  'prefix' => BootForm::addonIcon('key fas')]) !!}
     <div class="form-group">
         <button type="submit" class="btn btn-rounded btn-primary pull-right"><i class="fas fa-check icon"></i>@lang('badge.btn_verify_code')</button>
     </div>
