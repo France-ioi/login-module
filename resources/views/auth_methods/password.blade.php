96569dc08de1db398829c2ac7ac930cd80a44e6a
Melanie Storup <m.storup@epixode.fr>
2019-03-06 15:35:47 +0100

    fix  mssing auth_methods password template

---
+++ resources/views/auth_methods/password.blade.php
@@ -0,0 +1,18 @@
+<li class="list-group-item">
+    {{ trans('auth_methods.password') }}
+    @if($has_password)
+        <span class="label label-success">
+            <i class="fas fa-check"></i>
+            @lang('auth_methods.active')
+        </span>
+    @endif
+    <button type="button" class="btn-link pull-right" data-toggle="collapse" data-target="#edit-password" aria-expanded>
+        <span class="openCollapseItem"><i class="fas fa-pencil-alt icon"></i> @lang($has_password ? 'auth_methods.btn_change' : 'auth_methods.btn_add')</span>
+        <span class="closeCollapseItem"><i class="fas fa-times icon"></i> @lang('ui.cancel')</span>
+    </button>
+	<div class="collapse" id="edit-password">
+	    <div class="inline-form">
+	        @include('password.index')
+	    </div>
+	</div>
+</li>
