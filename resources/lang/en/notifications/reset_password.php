39c68480af06ac3e8ce408cc04385eb5ae995811
Michel Blockelet <blockelet@crans.org>
2020-02-10 23:31:27 +0000

    i18n: Update reset_password

--- resources/lang/en/notifications/reset_password.php
+++ resources/lang/en/notifications/reset_password.php
@@ -1,5 +1,14 @@
 <?php
 return [
-    'subject' => ":app_name password reset request",
-    'body' => "Hello :login! Your password reset token: \n:token\n-----\n:app_name"
+    'subject' => ":app_name - Password reset request",
+    'body' => "Hello,
+A recovery request for the account :login on the platform :app_name,
+associated with your email address, has been made.
+If you requested this recovery, here is the code to paste into the recovery
+interface in which you made the request :
+:token
+If you didn't make that request, you can ignore this message.
+Best regards,
+-- 
+The technical team"
 ];
--- resources/lang/fr/notifications/reset_password.php
+++ resources/lang/fr/notifications/reset_password.php
@@ -1,5 +1,14 @@
 <?php
 return [
     'subject' => ":app_name - Réinitialisation du mot de passe",
-    'body' => "Votre jeton de réinitialisation du mot de passe :\n:token"
+    'body' => "Bonjour,
+Une demande de récupération du compte :login sur la plateforme :app_name,
+qui est associé à votre adresse email, vient d'être effectuée.
+Si vous êtes bien l'auteur de cette demande, voici le code à coller dans
+l'interface de récupération depuis laquelle vous avez effectué cette demande :
+:token
+Si vous n'êtes pas à l'origine de cette demande, vous pouvez ignorer ce message.
+Bien cordialement,
+-- 
+L'équipe technique"
 ];
