2693983246fcbe0ff65afbe17aa8111f2f9c62b2
mobydimk <mobydimk@gmail.com>
2022-02-19 00:49:25 +0400

    client admin templates update

--- resources/views/client_admin/users/ban.blade.php
+++ resources/views/client_admin/users/ban.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.client_admin')
 
 @section('content')
-    <h3>User {{ $user->id }}</h3>
-    <p>{{ $user->first_name }} {{ $user->last_name }}</p>
+    @include('client_admin.users.user_card')
 
     {!! BootForm::open(['url' => '/client_admin/'.$client->id.'/users/'.$user->id.'/ban']) !!}
         <input type="hidden" name="refer_page" value="{{ $refer_page }}"/>
---
+++ resources/views/client_admin/users/user_card.blade.php
@@ -0,0 +1,2 @@
+<h3>User {{ $user->id }}</h3>
+<p>{{ $user->first_name }} {{ $user->last_name }}</p>
